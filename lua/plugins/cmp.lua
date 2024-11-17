eaf9c8c6668541d6d7893955d2276d761d6d056b
Chan Shi Yuan Galvin <galvin.chansy@shopee.com>
2024-11-17 16:15:54 +0800

    add plugins

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,11 +11,11 @@ return {
         local cmp = require("cmp")
         cmp.setup({
             mapping = cmp.mapping.preset.insert({
-                ['<C-Space>'] = cmp.mapping.complete(),
+                ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-k>"] = cmp.mapping.select_prev_item(),
                 ["<C-j>"] = cmp.mapping.select_next_item(),
-                ['<C-h>'] = cmp.mapping.abort(),
-                ['<C-l>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-x>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
