759c7f134e35cca53e40203522fe02bc867c702f
Chan Shi Yuan Galvin <galvin.chansy@shopee.com>
2024-11-17 17:44:22 +0800

    telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,11 +8,19 @@ return {
         }
     },
     config = function()
+        require("telescope").setup({
+            pickers = {
+                buffers = {
+                    initial_mode = "normal"
+                }
+            },
+        })
         local telescope_builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Telescope find files" })
         vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, { desc = "Telescope live grep" })
         vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Telescope buffers" })
         vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Telescope help tags" })
-        vim.keymap.set("n", "<leader>cc", telescope_builtin.colorscheme, { desc = "Telescope colorscheme" })
+        vim.keymap.set("n", "<leader>fc", telescope_builtin.colorscheme, { desc = "Telescope colorscheme" })
+        vim.keymap.set("n", "<leader>fs", telescope_builtin.grep_string, { desc = "Telescope grep string" })
     end
 }
